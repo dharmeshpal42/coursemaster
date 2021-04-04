48419aede29e75656232f8c19185e549726b1b53
dharmeshpal42 <dharmeshpal42@gmail.com>
2021-04-04 22:07:30 +0530

    getter setter changed

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -14,9 +14,52 @@ public class Course {
 	private int courseId;
 	private String courseName;
 	private int courseFees;
-	
 	public Course()
 	{
 		
 	}
+	/**
+	 * @return the courseId
+	 */
+	public int getCourseId() {
+		return courseId;
+	}
+	/**
+	 * @param courseId the courseId to set
+	 */
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+	/**
+	 * @param courseName the courseName to set
+	 */
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	/**
+	 * @return the courseFees
+	 */
+	public int getCourseFees() {
+		return courseFees;
+	}
+	/**
+	 * @param courseFees the courseFees to set
+	 */
+	public void setCourseFees(int courseFees) {
+		this.courseFees = courseFees;
+	}
+	@Override
+	public String toString() {
+		return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseFees=" + courseFees + "]";
+	}
+	
+	
+	
+	
 }
